16c305db9ee83aecbba0edaa56fe998e458c3137
lakshrajmofficial <153515908+lakshrajmofficial@users.noreply.github.com>
2023-12-11 23:29:30 +0530

    Delete src directory

--- src/components/Navbar Component/NavbarItem.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function NavbarItem (props) {
-  return(
-    <li className="nav-item">
-        <a className="nav-link" href={props.link}>{props.text}</a>
-    </li>
-  );
-}

3d1920b99de248197e28c7845a32ab08ac965225
lakshrajmofficial <lakshraj123@gmail.com>
2023-12-11 23:32:31 +0530

    Updates

---
+++ precache-manifest.fb084af4581931e0bbfdecdd48d2a6a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36447bb4cca3fb781f8767141da865d3",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "028106cadad246bf6f2f",
+    "url": "/TextUtils/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d6b45bec2d7defd9d581",
+    "url": "/TextUtils/static/js/2.ffaa2003.chunk.js"
+  },
+  {
+    "revision": "9b33a9281d9e324c810d17561ce0b881",
+    "url": "/TextUtils/static/js/2.ffaa2003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09709edb784633bbeda3",
+    "url": "/TextUtils/static/js/3.16547feb.chunk.js"
+  },
+  {
+    "revision": "028106cadad246bf6f2f",
+    "url": "/TextUtils/static/js/main.0ff12823.chunk.js"
+  },
+  {
+    "revision": "9e99c1bfefc3071dcfd7",
+    "url": "/TextUtils/static/js/runtime-main.e5550a99.js"
+  }
+]);
